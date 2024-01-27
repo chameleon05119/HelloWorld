a558b27b4a9a81982ff58e90999fb30d9d425ee2
chameleon05119 <chameleon05119@gmail.com>
2024-01-27 16:42:17 +0900

    画像の表示領域の制御の学習

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -10,15 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-            VStack(alignment: .trailing) {
-                Text("知性の自転車").font(.largeTitle).fontWeight(.black)
-                Text("Bicycle for the Mind.").italic().offset(x: -10, y: 0)
-            }.padding(.top, 80)
-            VStack(alignment: .trailing) {
-                Text("憐れみは恋の始まり").font(.largeTitle).fontWeight(.medium)
-                Text("Pity is akin to love.").italic().offset(x: -10, y: 0)
-            }.padding(.top, 20)
-            Spacer()
+            Image("test")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fill)
+                .scaleEffect(1.8)
+                .offset(x: 70, y: -20)
+                .frame(width: 300, height: 500)
+                .clipped()
+            Text("Hello World!").padding()
         }
     }
 }
