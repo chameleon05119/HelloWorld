2e85d0aa028ff5533e1cd5e052e3f7971378d1f3
chameleon05119 <chameleon05119@gmail.com>
2024-01-27 16:22:15 +0900

    スペーサーの学習

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe").imageScale(.large).foregroundColor(.accentColor)
-            Text("Hello World!").padding(.vertical, 30.0).padding(.horizontal, 50).background(Color.yellow)
+        VStack{
+            VStack(alignment: .trailing) {
+                Text("知性の自転車").font(.largeTitle).fontWeight(.black)
+                Text("Bicycle for the Mind.").italic().offset(x: -10, y: 0)
+            }.padding(.top, 80)
+            VStack(alignment: .trailing) {
+                Text("憐れみは恋の始まり").font(.largeTitle).fontWeight(.medium)
+                Text("Pity is akin to love.").italic().offset(x: -10, y: 0)
+            }.padding(.top, 20)
+            Spacer()
         }
     }
 }
